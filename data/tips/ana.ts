943864dcb7eeaa9f2b001f4f245c848dc3538745
Matthew Hatcher <matthewh@halfpetal.com>
2020-04-23 08:52:36 -0400

    Adding some data.

---
+++ data/tips/ana.ts
@@ -0,0 +1,9 @@
+import { authors } from "./base";
+
+export const tips = {
+    '0-crosshair': {
+        'author': authors.vale,
+        'short': 'Turn your crosshair opacity to 0.',
+        'long': 'dnshfksdfkdlsfkjdskjfjkldskjlflkjdjlkfljdskjflkjdsjkfjdksfkjlsdlkjfjkldkjlfjklsdjflksjlkdfjkdskjlf'
+    }
+}
---
+++ data/tips/base.ts
@@ -0,0 +1,18 @@
+// {
+//     "available_heroes": ["ana"],
+//     "authors": {
+//         "vale": {
+//             "image": null,
+//             "link": "https://twitch.tv/Vale"
+//         }
+//     }
+// }
+
+export const available_heroes = ['ana'];
+
+export const authors = {
+    'vale': {
+        'image': null,
+        'link': 'https://twitch.tv/Vale'
+    }
+};

a545d108d7fdbf5a03027018b6984962792fd6e5
Matthew Hatcher <matthewh@halfpetal.com>
2020-04-23 11:51:37 -0400

    Home page layout.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,34 @@
 import Layout from '../components/layout';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import { tips } from '../data/tips/tips';
+import Link from 'next/link'
+
+const homeImageGrid = {
+    backgroundColor: 'rgba(0, 0, 0, 0.8)'
+};
 
 const Home = () => (
     <Layout title="Home">
-      <h1>Hello</h1>
+        <Row>
+            {Object.keys(tips).map((key: any) => (
+                <Link href='/hero/[hid]' as={`/hero/${key}`}>
+                    <a>
+                    <Col md={3} sm={12}>
+                        <Card className='bg-dark text-white'>
+                            <Card.Img src={tips[key].meta.profile_image}></Card.Img>
+                            <Card.ImgOverlay style={homeImageGrid} className='d-flex'>
+                                <div className='my-auto mx-auto text-center'>
+                                    <Card.Title className='display-4'>{tips[key].meta.display_name}</Card.Title>
+                                </div>
+                            </Card.ImgOverlay>
+                        </Card>
+                    </Col>
+                    </a>
+                </Link>
+            ))}
+        </Row>
     </Layout>
 );
 

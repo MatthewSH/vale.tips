b9f4f4920a22f078925cca2aeb7478d7c8807bd1
Matthew Hatcher <matthewh@halfpetal.com>
2020-04-23 08:48:51 -0400

    Changing header navigation.

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -11,18 +11,17 @@ const Navigation = () => (
             height='30'
             className='d-inline-block align-top'
             />{' '}
-            Paladins Sidekick
+            Vale Tips
         </Navbar.Brand>
         <Navbar.Toggle aria-controls='responsive-navbar-nav' />
         <Navbar.Collapse id='responsive-navbar-nav'>
             <Nav className='mr-auto'>
                 <Nav.Link href='/'>Home</Nav.Link>
-                <Nav.Link href='https://discordapp.com/oauth2/authorize?client_id=691669920093700116&permissions=536345936&scope=bot' target='_blank'>Invite the Bot</Nav.Link>
-                <Nav.Link href='/commands'>Command List</Nav.Link>
             </Nav>
             <Nav>
-                <Nav.Link href='https://discord.gg/cyRugRa' target='_blank'>Join Us On Discord</Nav.Link>
-                <Nav.Link href='https://twitter.com/PaladinsDiscord' target='_blank'>Follow Us on Twitter</Nav.Link>
+                <Nav.Link href='' target='_blank'>Twitch</Nav.Link>
+                <Nav.Link href='' target='_blank'>Twitter</Nav.Link>
+                <Nav.Link href='' target='_blank'>Discord</Nav.Link>
             </Nav>
         </Navbar.Collapse>
     </Navbar>

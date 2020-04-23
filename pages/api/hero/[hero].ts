516a34bec362723c595c16e4d44dbb1ef83d82a8
Matthew Hatcher <matthewh@halfpetal.com>
2020-04-23 09:29:18 -0400

    Sending all hero data.

---
+++ pages/api/hero/[hero].ts
@@ -0,0 +1,16 @@
+import { tips } from '../../../data/tips/tips';
+
+const Hero = (req, res) => {
+    const {
+        query: { hero }, 
+    } = req;
+
+    if (!tips[hero]) {
+        return res.status(404).json({ error: 'Hero not available.'})
+    }
+    let heroData = tips[hero];
+
+    res.json(heroData);
+};
+
+export default Hero;
